5aea74213afa24e79b08a4265938fdefb12b4e37
BALAJI HARIHARAN N S <balajihariharanns@gmail.com>
2026-02-23 20:30:28 +0530

    Implement balanced parentheses check using Stack
    Added a second method to check for balanced parentheses using a stack.

--- valid.java
+++ valid.java
@@ -21,3 +21,48 @@ public class Main {
         System.out.println(stack.isEmpty() ? "Balanced" : "Not Balanced");
     }
 }
+
+
+
+2nd way 
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String str = "({[}])";
+		Stack<Character> stack = new Stack<>();
+		boolean valid = true;
+		for(char ch:str.toCharArray())
+		{
+		    if(ch=='{' || ch=='[' || ch=='(')
+		    {
+		        stack.push(ch);
+		    }
+		    else
+		    {
+		        if(stack.isEmpty())
+		        {
+		            valid = false;
+		            break;
+		        }
+		        char top = stack.pop();
+		        if((ch==']' && top!='[') || (ch=='}' && top!='{') || (ch==')' && top!='('))
+		        {
+		            valid = false;
+		            break;
+		        }
+		    }
+		}
+		if(!stack.isEmpty())
+		{
+		    valid = false;
+		    System.out.println(valid);
+		}
+		else
+		{
+		    System.out.println(valid);
+		}
+	}
+}
